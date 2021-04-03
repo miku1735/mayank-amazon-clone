5e6c11601a814cef6883c314e6dd1b43ecc0a3bb
Mayank <Mayank13@ibm.com>
2021-04-03 12:31:38 +0530

    Updates

---
+++ precache-manifest.4e82093d2a4d812e43405150b3a0f8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700b47aeedaad0365ea8bd04181221db",
+    "url": "/mayank-amazon-clone/index.html"
+  },
+  {
+    "revision": "cbd38ad536d25f3ef6bd",
+    "url": "/mayank-amazon-clone/static/css/main.43a4057b.chunk.css"
+  },
+  {
+    "revision": "ecea41b08a7d48f81cbe",
+    "url": "/mayank-amazon-clone/static/js/2.e80de50b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mayank-amazon-clone/static/js/2.e80de50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd38ad536d25f3ef6bd",
+    "url": "/mayank-amazon-clone/static/js/main.42635f4d.chunk.js"
+  },
+  {
+    "revision": "2b93768bd2d2e12529ff",
+    "url": "/mayank-amazon-clone/static/js/runtime-main.01d26f10.js"
+  },
+  {
+    "revision": "35fb9164ed5392832341e7d64b2f5a67",
+    "url": "/mayank-amazon-clone/static/media/amazon-logo.35fb9164.png"
+  }
+]);
